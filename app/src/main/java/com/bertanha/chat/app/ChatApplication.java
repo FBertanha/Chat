9b78e187a2a64d2be9ef6b95dfb60ee0eb60935b
Felipe Bertanha <bertanhahp@hotmail.com>
2017-09-17 15:08:21 -0300

    Aula 3

--- app/src/main/java/com/bertanha/chat/app/ChatApplication.java
+++ app/src/main/java/com/bertanha/chat/app/ChatApplication.java
@@ -1,8 +1,25 @@
 package com.bertanha.chat.app;
 
+import android.app.Application;
+
+import com.bertanha.chat.service.ChatService;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by Felipe on 17/09/2017.
  */
 
-public class ChatApplication {
+public class ChatApplication extends Application{
+
+    public ChatService getChatService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.103:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ChatService chatService = retrofit.create(ChatService.class);
+        return chatService;
+    }
 }
